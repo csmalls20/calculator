f6a7b3d235665a728c72573d36c2e0a8200363a7
Christopher Smalls <90156757+csmalls20@users.noreply.github.com>
2025-07-07 15:34:27 -0400

    Create function that calls unique operation
    Created a function that takes two numbers and an operator as parameters and calls one of the basic math functions depending on the operator chosen.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum;
+let secondNum;
+let operator;
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +16,21 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+      break;
+  }
 }
